36bf744eb6525de9f71ea6f34df44a857af0c997
Justin <justinvanheek@gmail.com>
2017-07-31 17:42:00 -0700

    Initial commit

---
+++ app/src/main/java/uwai/dev/uwai/MainActivity.java
@@ -0,0 +1,64 @@
+package uwai.dev.uwai;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView)findViewById(R.id.WebView);
+
+        webview.clearCache(true);
+        CookieSyncManager.createInstance(this);
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeAllCookie();
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                goToNoNetworkError(view);
+            }
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if(url.contains("138.197.129.141")) {
+                    if (isNetworkAvailable()) {
+                        view.loadUrl(url);
+                    }
+                    else {
+                        goToNoNetworkError(view);
+                    }
+                }
+                return true;
+            }
+        });
+        if (isNetworkAvailable()) {
+            webview.loadUrl("http://138.197.129.141/uwai/");
+        }
+        else {
+            goToNoNetworkError(webview);
+        }
+
+    }
+
+    private void goToNoNetworkError(WebView view) {
+        view.loadUrl("file:///android_asset/NoNetworkError.html");
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}
